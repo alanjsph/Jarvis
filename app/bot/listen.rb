fa73575013178f0df6c92430addc2702316c81ca
Alan Joseph <alanjsph@live.com>
2017-03-19 14:38:47 +0530

    nothing commits

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -1,5 +1,6 @@
 require "facebook/messenger"
 include Facebook::Messenger
+require 'json'
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 
@@ -8,10 +9,11 @@ Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 # message.sent_at     # => 2016-04-22 21:30:36 +0200
 # message.text        # => 'Hello, bot!'
 Bot.on :message do |message|
+  puts message.sender["id"]
   Bot.deliver({
     recipient: message.sender,
     message: {
-      text: message.text
+      text: message.text + ". This is Alan's AI Assistant"
     }
   }, access_token: ENV["ACCESS_TOKEN"])
 end
